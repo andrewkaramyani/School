2926370fff1d52e6d88e75e1b4a70cb8a59e637c
andrewkaramyani <65507230+andrewkaramyani@users.noreply.github.com>
2020-07-11 12:45:05 +0200

    Add project files.

---
+++ School/Controllers/TeacherController.cs
@@ -0,0 +1,82 @@
+﻿using School.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace School.Controllers
+{
+    [RoutePrefix("school")]
+    public class TeacherController : Controller
+    {
+
+        private static List<Teacher> Teachers = new List<Teacher>()
+        {
+            new Teacher { Id = 1,Name="iti", BlogUrl = "http://www.iti.com",
+                DateOfBirth=DateTime.Now.AddYears(-30),Email="andrewnashat47@yahoo.com",
+            Mobile="01289855751", NationalId="01234567891234",Passowrd="1234a@",Salary=6000
+            ,ismarried=true},
+        };
+
+        public ActionResult Index()
+        {
+            return View(Teachers);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Teachers.RemoveAll(e => e.Id == id);
+            return RedirectToAction("index");
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        public ActionResult Create(Teacher teach)
+        {
+            if (ModelState.IsValid)
+            {
+                teach.Id = Teachers.Max(x => x.Id) + 1;
+                Teachers.Add(teach);
+                return RedirectToAction("index");
+            }
+            else
+            {
+                return View("Create");
+            }
+  
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var T = Teachers.FirstOrDefault(x => x.Id == id);
+            return View(T);
+        }
+        [HttpPost]
+        public ActionResult Edit(Teacher tech)
+        {
+            if (ModelState.IsValid)
+            {
+                Teachers.RemoveAll(x => x.Id == tech.Id);
+                Teachers.Add(tech);
+                return RedirectToAction("index");
+            }
+            else
+            {
+                return View(tech);
+            }
+            
+        }
+
+    }
+      
+
+   
+}
---
+++ School/Models/Teacher.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace School.Models
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+        
+        [Display(Name = "Date Of Birth")]
+        public DateTime? DateOfBirth { get; set; }
+        
+        
+        [Url]
+        [Display(Name = "Blog Url")]
+        public string BlogUrl { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+        
+        [Phone]
+        public string Mobile { get; set; }
+        
+        [RegularExpression(".{6,}",ErrorMessage ="Password should be more than 6 char")]
+        [DataType(DataType.Password)]
+        public string Passowrd { get; set; }
+        
+        [Display(Name = "National Code")]
+        [RegularExpression("[0-9]{14}")]
+        [Required]
+        public string NationalId { get; set; }
+
+        [Range(500, 10000)]
+        [DataType(DataType.Currency)]
+        public double? Salary { get; set; }
+
+        public bool? ismarried { get; set; }
+      
+    }
+}
